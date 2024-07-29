c94eabaa845c9a1f3fe6433db66e15b929313364
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-29 17:21:33 +0100

    refactor: Update ProductGrid component CSS class name
    The commit updates the CSS class name in the ProductGrid component from "grid" to "grid-cols" to align with the Tailwind CSS naming convention. This change improves the consistency and maintainability of the codebase by using standardized class names.

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -5,7 +5,7 @@ interface ProductGridProps {
 }
 export const ProductGrid = ({ products }: ProductGridProps) => {
   return (
-    <div className="grid grid-col-2 sm:grid-col-3 gap-10 mb-10">
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
       {products.map((product) => {
         return <span key={product.slug}>{product.title}</span>;
       })}
