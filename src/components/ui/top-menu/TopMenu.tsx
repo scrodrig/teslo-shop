a1eef1b7117456108c8fdd205c333a770b4b3f87
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-29 11:51:21 +0100

    feat: Add TopMenu component for consistent navigation
    This commit adds the TopMenu component to the ShopLayout, allowing for a consistent navigation experience throughout the shop pages. The TopMenu component is now exported from the components module and rendered above the children components in the main element. This change enhances the user experience by providing easy access to important navigation links. Additionally, the react-icons dependency has been added to enable the use of icons in various components throughout the application.

---
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -0,0 +1,63 @@
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+
+import Link from "next/link";
+import React from "react";
+import { titleFont } from "@/config/fonts";
+
+export const TopMenu = () => {
+  return (
+    <nav className="flex px-5 justify-between items-center w-full">
+      {/* {Logo} */}
+      <div>
+        <Link href="/">
+          <span className={`${titleFont.className} antialiased font-bold`}>
+            Teslo
+          </span>
+          <span> | Shop</span>
+        </Link>
+      </div>
+
+      {/* {Top Menu} */}
+      <div className="hidden sm:block">
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/men"
+        >
+          Men
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/women"
+        >
+          Women
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/kids"
+        >
+          Kids
+        </Link>
+      </div>
+
+      {/* Seach, {Cart}, Menu */}
+      <div className="flex items-center">
+        <Link href="/search">
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+
+        <Link href="/cart">
+          <div className="relative">
+            <span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5" />
+          </div>
+        </Link>
+
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          Menu
+        </button>
+      </div>
+    </nav>
+  );
+};
