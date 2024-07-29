731d06b0a17259ec905beddb9e2bc1f84e23765e
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-29 12:55:49 +0100

    refactor: Update ShopLayout component to include padding for children
    This commit updates the ShopLayout component in the src/app/(shop)/layout.tsx file to include a padding of 10 pixels on the left and right sides of the children components. This change improves the layout of the shop pages by providing consistent spacing between the content and the edges of the page.

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -8,7 +8,7 @@ export default function ShopLayout({
   return (
     <main className="min-h-screen">
       <TopMenu />
-      {children}
+      <div className="px-10">{children}</div>
     </main>
   );
 }
