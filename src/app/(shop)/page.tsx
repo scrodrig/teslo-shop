ea3d5014294aa6abdfceda8c121f228774530824
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-29 12:55:53 +0100

    feat: Add Title component to Shop page
    The commit adds the Title component to the Shop page in the src/app/(shop)/page.tsx file. This change improves the page layout by displaying a title and subtitle for the shop page.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,10 @@
+import { Title } from "@/components";
 import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hello world</h1>
-      <h1 className={`${titleFont.className}`}>Hello world</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hello world</h1>
-    </div>
+    <>
+      <Title title="Shop" subtitle="All products" className="mb-2" />
+    </>
   );
 }
