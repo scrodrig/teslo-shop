1a9d1581127b735348677b788ce6d91a559b79d4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 10:40:10 +0100

    feat: Add lazy loading for images in ProductPage component

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,42 @@
-export default function ProductPage() {
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+import { titleFont } from "@/config/fonts";
+
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params: { slug } }: ProductPageProps) {
+  console.log("slug", slug);
+
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      {/*Slideshow */}
+      <div className="col-span-1 md:col-span-2">Hello</div>
+
+      {/* Details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiases font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* Size selector */}
+        {/* Quantity selector */}
+        {/* Button */}
+        <button className="btn-primary my-5">Add to cart</button>
+        {/* Description */}
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
