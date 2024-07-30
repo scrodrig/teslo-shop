595e9f8e3c0010cabc90f3d9c51152bbfb276ab6
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 08:44:35 +0100

    feat: Add openMenu function to TopMenu component
    This commit adds the openMenu function to the TopMenu component in the src/components/ui/top-menu/TopMenu.tsx file. The openMenu function is called when the "Menu" button is clicked and it triggers the opening of the side menu. This change enhances the functionality of the application by providing a way for users to easily access the side menu.

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 import Link from "next/link";
 import React from "react";
 import { titleFont } from "@/config/fonts";
+import { useUiStore } from "@/store";
 
 export const TopMenu = () => {
+  const openMenu = useUiStore((state) => state.openSideMenu);
   return (
     <nav className="flex px-5 justify-between items-center w-full">
       {/* {Logo} */}
@@ -54,7 +58,10 @@ export const TopMenu = () => {
           </div>
         </Link>
 
-        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+        <button
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          onClick={() => openMenu()}
+        >
           Menu
         </button>
       </div>
