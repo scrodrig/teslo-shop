938d7d50256f42dd8bb2b00e83d5d97751e3be26
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 10:48:55 +0100

    feat: Add SizeSelector component to ProductPage

---
+++ src/components/product/size-selector/SizeSelector.tsx
@@ -0,0 +1,30 @@
+import { Size } from "@/interfaces";
+import clsx from "clsx";
+
+interface Props {
+  selectedSize: Size;
+  availableSizes: Size[];
+}
+
+export const SizeSelector = ({ selectedSize, availableSizes }: Props) => {
+  return (
+    <div className="my-5">
+      <h3 className="font-bold mb-4">Available Sizes</h3>
+
+      <div className="flex">
+        {availableSizes.map((size) => {
+          return (
+            <button
+              key={size}
+              className={clsx("mx-2 hover:underline text-lg", {
+                underline: selectedSize === size,
+              })}
+            >
+              {size}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
