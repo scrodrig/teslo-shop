4fd49ec8470e1080bd7e387e3e5c423aa36e114c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 08:44:39 +0100

    feat: Add lazy loading for images in Sidebar component

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -12,21 +12,38 @@ import {
 } from "react-icons/io5";
 
 import Link from "next/link";
+import clsx from "clsx";
+import { useUiStore } from "@/store";
 
 export const Sidebar = () => {
+  const isSideMenuOpen = useUiStore((state) => state.isSideMenuOpen);
+  const closeMenu = useUiStore((state) => state.closeSideMenu);
+
   return (
     <div>
       {/* Black background */}
-      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"></div>
-      {/* Blur */}
-      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"></div>
+      {isSideMenuOpen && (
+        <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"></div>
+      )}
 
+      {/* Blur */}
+      {isSideMenuOpen && (
+        <div
+          className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+          onClick={() => closeMenu()}
+        ></div>
+      )}
       {/* Sidebar */}
-      <nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
+      <nav
+        className={clsx(
+          "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          { "translate-x-full": !isSideMenuOpen }
+        )}
+      >
         <IoCloseOutline
           size={50}
           className="absolute top-5 right-5 cursor-pointer"
-          onClick={() => console.log("close")}
+          onClick={() => closeMenu()}
         />
         {/*Input*/}
         <div className="relative mt-14">
@@ -75,7 +92,6 @@ export const Sidebar = () => {
         {/*Line separator*/}
         <div className="w-full h-px bg-gray-200 my-10" />
 
-
         <Link
           href="/"
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
@@ -99,7 +115,6 @@ export const Sidebar = () => {
           <IoPeopleOutline size={30} />
           <span className="ml-3 text-xl">Users</span>
         </Link>
-
       </nav>
     </div>
   );
