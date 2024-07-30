9ebe28fc7023d9975a73d78cbbe79d343e778087
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 09:58:24 +0100

    refactor: Update product interface properties
    This commit updates the properties of the Product interface in the src/interfaces/product.interface.ts file. It changes the "sizes" property from "ValidSizes[]" to "ValidSize[]", and the "type" property from "ValidTypes" to "ValidType". This change ensures consistency and clarity in the naming conventions of the interface properties.

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -3,18 +3,17 @@ export interface Product {
   images: string[];
   inStock: number;
   price: number;
-  sizes: ValidSizes[];
+  sizes: ValidSize[];
   slug: string;
   tags: string[];
   title: string;
-  type: ValidTypes;
-  gender: 'men'|'women'|'kid'|'unisex'
+  type: ValidType;
+  gender: ValidCategory;
 }
 
-export type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
-export type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
+export type ValidCategory = "men" | "women" | "kid" | "unisex";
+export type ValidSize = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
+export type ValidType = "shirts" | "pants" | "hoodies" | "hats";
 export interface SeedData {
-  products: Product[],
+  products: Product[];
 }
-
-
