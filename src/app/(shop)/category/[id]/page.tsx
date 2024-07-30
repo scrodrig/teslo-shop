9b040719d14cc4741dd5496d703813858c645433
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-30 09:58:19 +0100

    feat: Add lazy loading for images in CategoryPage component

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,19 +1,40 @@
+import { ProductGrid, Title } from "@/components";
+
+import { ValidCategory } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface CategoryPageProps {
   params: {
-    id: string;
+    id: ValidCategory;
   };
 }
 
+const seedProducts = initialData.products;
+
 export default function CategoryPage({ params: { id } }: CategoryPageProps) {
-  if (id === "kids") {
-    notFound();
-  }
+  // if (id === "kids") {
+  //   notFound();
+  // }
+
+  const products = seedProducts.filter((product) => product.gender === id);
+
+  const labels: Record<ValidCategory, string> = {
+    men: "Men",
+    women: "Women",
+    kid: "Kids",
+    unisex: "Unisex",
+  };
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`${labels[id]} Articles`}
+        subtitle="All products"
+        className="mb-2"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
