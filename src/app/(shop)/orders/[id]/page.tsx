0d5af7e6d0e09a253317259bd5cca171fff5e2aa
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 07:01:37 +0100

    refactor: Improve responsive layout of OrdersByIdPage
    This commit improves the responsive layout of the OrdersByIdPage by introducing CSS classes for centering and adjusting the layout of the elements. It also adds a responsive grid for the shopping cart section and updates the delivery address and order summary sections to be more visually appealing on different screen sizes. These updates enhance the user experience by ensuring that the OrdersByIdPage is properly displayed and accessible on various devices.

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,7 +1,104 @@
-export default function OrdersByIdPage() {
+import { QuantitySelector, Title } from "@/components";
+
+import Image from "next/image";
+import { IoCartOutline } from "react-icons/io5";
+import Link from "next/link";
+import clsx from "clsx";
+import { initialData } from "@/seed/seed";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+interface OrdersByPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function OrdersByIdPage({ params: { id } }: OrdersByPageProps) {
   return (
-    <div>
-      <h1>Orders specific Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title={`Order #${id}`} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Shopping cart */}
+          <div className="flex flex-col mt-5">
+            <div
+              className={clsx(
+                "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                { "bg-red-500": false, "bg-green-800": true }
+              )}
+            >
+              <IoCartOutline size={30} />
+              {/* <span>Payment pending</span> */}
+              <span>Payment done</span>
+            </div>
+            {/* Product list */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  className="mr-5 rounded"
+                  style={{ width: "100px", height: "100px" }}
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <p className="font-bold">Subtotal: ${product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Delivery address</h2>
+            <div className="mb-10">
+              <p className="text-xl">John Doe</p>
+              <p>Av. Siempre Viva 123</p>
+              <p>Col. Central</p>
+              <p>Alcaldia Cauctemoc</p>
+              <p>Porto</p>
+              <p>4000-123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"></div>
+
+            <h2 className="text-2xl mb-2">Order Summary</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 articles</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+
+              <span>Impuestos</span>
+              <span className="text-right">$23</span>
+
+              <span className="text-xl font-bold mt-5">Total:</span>
+              <span className="text-xl mt-5 text-right">$123</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <div
+                className={clsx(
+                  "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                  { "bg-red-500": false, "bg-green-800": true }
+                )}
+              >
+                <IoCartOutline size={30} />
+                {/* <span>Payment pending</span> */}
+                <span>Payment done</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
