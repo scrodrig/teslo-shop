df04815168ac412002c8becb3138764b47216c09
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 07:23:14 +0100

    refactor: Update AuthLayout component to improve responsive layout
    This commit updates the AuthLayout component to improve its responsive layout. The changes include introducing CSS classes for centering and adjusting the layout of the elements. The main content is now wrapped in a div with a maximum width of 350px, ensuring that it is properly displayed on different screen sizes. This improvement enhances the user experience by providing a visually consistent layout on the Auth pages.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,11 @@
-export default function ShopLayout({
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="bg-gray-500 min-h-screen">{children}</main>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
