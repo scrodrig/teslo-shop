02ed01d3607f598952be9fd274b2eea3e46cdabf
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 16:10:14 +0100

    feat: Add pagination functionality for retrieving paginated products with images

---
+++ src/actions/product/product-pagination.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
+  //! 1. Get products with images
+  try {
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+  //! 2. Total of pages
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
+
+
+    return {
+      currentPage: page,
+      totalPages,
+      products: products.map((product) => {
+        return {
+          ...product,
+          images: product.ProductImage.map((image) => image.url),
+        };
+      }),
+    };
+  } catch (e) {
+    throw new Error("Products not loaded");
+  }
+};
