2b8eeba88aa3bc99c9576ff68e2cda9737a430cf
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 16:58:42 +0100

    feat: Update Pagination component to handle page navigation
    The code changes in this commit update the Pagination component to handle page navigation. The component now uses the `usePathname` and `useSearchParams` hooks from the `next/navigation` package to get the current path and search parameters. It also adds a `createPageUrl` function to generate the URL for each page based on the current path and search parameters. These changes enhance the functionality of the Pagination component by allowing users to navigate between pages more easily.

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,12 +1,38 @@
-import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5"
+'use client'
 
-import Link from "next/link"
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+import Link from 'next/link'
+import path from 'path'
 
 interface Props {
   totalPages: number
 }
 
 export const Pagination = ({ totalPages }: Props) => {
+  const pathName = usePathname()
+  const searchParams = useSearchParams()
+  const currentPage = Number(searchParams.get('page')) ?? 1
+
+  const createPageUrl = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams)
+    if (pageNumber === '...') {
+      return `${pathName}?${params.toString()}`
+    }
+
+    if (+pageNumber <= 0) {
+      return `${pathName}`
+    }
+
+    if (+pageNumber > totalPages) {
+      return `${pathName}?${params.toString()}`
+    }
+
+    params.set('page', pageNumber.toString())
+    return `${pathName}?${params.toString()}`
+  }
+
   return (
     <div className="flex text-center justify-center mt-10 mb-32">
       <nav aria-label="Page navigation example">
@@ -14,7 +40,7 @@ export const Pagination = ({ totalPages }: Props) => {
           <li className="page-item">
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#">
+              href={createPageUrl(currentPage - 1)}>
               <IoChevronBackOutline size={30} />
             </Link>
           </li>
@@ -42,7 +68,7 @@ export const Pagination = ({ totalPages }: Props) => {
           <li className="page-item">
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#">
+              href={createPageUrl(currentPage + 1)}>
               <IoChevronForwardOutline size={30} />
             </Link>
           </li>
