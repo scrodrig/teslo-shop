d48e960254a926bf095679a9cb09502c7221a921
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 15:09:54 +0100

    refactor: Update seed-database.ts for database seeding
    This commit refactors the "seed-database.ts" file to improve the process of seeding the database with initial data. It removes the commented out code for deleting data and instead uses the Prisma deleteMany() method to delete existing records from the "productImage", "product", and "category" tables. This change ensures that the database is properly cleared before adding new data. Additionally, it introduces a more efficient approach for adding categories and products by using the Prisma createMany() method. These improvements enhance the reliability and performance of the database seeding process.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -4,21 +4,51 @@ import prisma from "../lib/prisma";
 
 async function main() {
   //? Delete data
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ]);
+  // await Promise.all([
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  // ]);
 
   //? Add a category
 
   const { categories, products } = initialData;
 
   const categoriesData = categories.map((category) => ({ name: category }));
-  
+
   await prisma.category.createMany({
-    data: categoriesData
+    data: categoriesData,
   });
+
+  const categoriesDb = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDb.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+  console.log("🚀 ~ categoriesMap ~ categoriesMap:", categoriesMap);
+
+  // const {images, type, ...product1} = products[0];
+
+  products.forEach(async (product) => {
+    const { images, type, ...productData } = product;
+    const productDb = await prisma.product.create({
+      data: {
+        ...productData,
+        categoryId: categoriesMap[type.toLowerCase()],
+      },
+    });
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: productDb.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  //? Add image
 
   console.log("Seed executed...");
 }
