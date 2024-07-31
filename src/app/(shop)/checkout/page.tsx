41b9da5871688b676935f4c14110fd79a6b39217
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-07-31 06:52:16 +0100

    feat: Improve responsive layout of checkout page
    This commit improves the responsive layout of the checkout page by introducing CSS classes for centering and adjusting the layout of the elements. It also adds a responsive grid for the shopping cart section and updates the delivery address and order summary sections to be more visually appealing on different screen sizes. These updates enhance the user experience by ensuring that the checkout page is properly displayed and accessible on various devices.

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,91 @@
-export default function CheckouPage() {
+import { QuantitySelector, Title } from "@/components";
+
+import Image from "next/image";
+import Link from "next/link";
+import { initialData } from "@/seed/seed";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+export default function CheckoutPage() {
   return (
-    <div>
-      <h1>Checkou Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Check Order" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Shopping cart */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Adjust elements</span>
+            <Link href="/" className="underline mb-5">
+              Edit shopping cart
+            </Link>
+            {/* Product list */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  className="mr-5 rounded"
+                  style={{ width: "100px", height: "100px" }}
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <p className="font-bold">Subtotal: ${product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Delivery address</h2>
+            <div className="mb-10">
+              <p className="text-xl">John Doe</p>
+              <p>Av. Siempre Viva 123</p>
+              <p>Col. Central</p>
+              <p>Alcaldia Cauctemoc</p>
+              <p>Porto</p>
+              <p>4000-123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"></div>
+
+            <h2 className="text-2xl mb-2">Order Summary</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 articles</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100</span>
+
+              <span>Impuestos</span>
+              <span className="text-right">$23</span>
+
+              <span className="text-xl font-bold mt-5">Total:</span>
+              <span className="text-xl mt-5 text-right">$123</span>
+            </div>
+
+            <p className="text-xs">
+              {`By clicking on Place Order" you agree to our `}
+              <Link href="#" className="underline">terms and conditions</Link>
+            </p>
+
+            <div className="mt-5 mb-2 w-full">
+              <Link
+                href="/orders/123"
+                className="flex justify-center btn-primary"
+              >
+                Place Order
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
