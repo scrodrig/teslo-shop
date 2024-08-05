3756fccd950bb9970e28df713f999b27f281b67e
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 11:29:19 +0100

    feat: Update metadata title template and default value
    The code changes in this commit update the metadata title template and default value in the layout.tsx file. The title template now includes a placeholder for dynamic content, and the default value is set to "Home | Testlo shop". This enhancement improves the flexibility and customization of the title displayed in the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import "./globals.css";
+import './globals.css'
 
-import type { Metadata } from "next";
-import { inter } from "@/config/fonts";
+import type { Metadata } from 'next'
+import { inter } from '@/config/fonts'
 
 export const metadata: Metadata = {
-  title: "Testlo shop",
-  description: "A virtual shop for testing",
-};
+  title: {
+    template: '%s | Testlo shop',
+    default: 'Home | Testlo shop',
+  },
+  description: 'A virtual shop for testing',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={inter.className}>{children}</body>
+      <body suppressHydrationWarning={true} className={inter.className}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
