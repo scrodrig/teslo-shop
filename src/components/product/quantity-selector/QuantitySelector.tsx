2734995e1718beb11a12e993db9fd31234bd1fa0
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 16:51:42 +0100

    refactor: Update QuantitySelector component to handle onQuantityChange prop
    This commit refactors the QuantitySelector component in the src/components/product/quantity-selector/QuantitySelector.tsx file to handle the onQuantityChange prop. The onQuantityChange prop is now passed as a prop to the QuantitySelector component, allowing for a callback function to be executed when the quantity is changed. This refactor improves the flexibility and reusability of the QuantitySelector component.

--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -1,28 +1,26 @@
-"use client";
+// "use client";
 
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
-import { useState } from "react";
-
 interface Props {
   quantity: number;
+  onQuantityChange: (value: number) => void;
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
-  const [count, setCount] = useState(quantity);
+export const QuantitySelector = ({ quantity, onQuantityChange }: Props) => {
 
-  const onQuantityChange = (value: number) => {
-    if(count + value < 1) return;
-    setCount(count + value);
+  const onValueChange = (value: number) => {
+    if(quantity + value < 1) return;
+    onQuantityChange(quantity + value);
   };
 
   return (
     <div className="flex">
-      <button onClick={() => onQuantityChange(-1)}>
+      <button onClick={() => onValueChange(-1)}>
         <IoRemoveCircleOutline size={30} />
       </button>
-      <span className="w-20 mx-3 bg-gray-100 text-center rounded">{count}</span>
-      <button onClick={() => onQuantityChange(+1)}>
+      <span className="w-20 mx-3 bg-gray-100 text-center rounded">{quantity}</span>
+      <button onClick={() => onValueChange(+1)}>
         <IoAddCircleOutline size={30} />
       </button>
     </div>
