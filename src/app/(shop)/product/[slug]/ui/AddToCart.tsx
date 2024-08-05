15bf0878698d2503f1bd53db3a389eea55ed5383
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 16:51:46 +0100

    feat: Add quantity and size validation to AddToCart component
    This commit adds quantity and size validation to the AddToCart component in the AddToCart.tsx file. When the user clicks on the "Add to cart" button, the component checks if a size is selected and displays an error message if not. Additionally, the component now tracks the quantity of the product and prevents adding it to the cart if the quantity is zero. This feature enhances the user experience by ensuring that valid size and quantity values are selected before adding the product to the cart.

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -13,16 +13,24 @@ export const AddToCart = ({ product }: Props) => {
 
 
   const [size, setSize] = useState<Size|undefined>()
+  const [quantity, setQuantity] = useState<number>(1)
+  const [posted, setPosted] = useState<boolean>(false)
 
+  const addToCart = () => {
+    setPosted(true)
+    if(!size) return
+    console.log('Add to cart', product, size, quantity)
+  }
 
   return (
     <>
+      {posted && !size && <p className="text-red-500 fade-in">Please select a size</p>}
       {/* Size selector */}
       <SizeSelector availableSizes={product.sizes} selectedSize={size} onSizeChange={setSize} />
       {/* Quantity selector */}
-      <QuantitySelector quantity={0} />
+      <QuantitySelector quantity={quantity} onQuantityChange={setQuantity}/>
       {/* Button */}
-      <button className="btn-primary my-5">Add to cart</button>
+      <button className="btn-primary my-5" onClick={addToCart}>Add to cart</button>
     </>
   )
 }
