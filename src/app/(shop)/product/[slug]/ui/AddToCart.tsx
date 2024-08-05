395df270b1fab2ef5466d1ca93c4854d4d29d2eb
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 17:24:17 +0100

    ```text refactor: Update AddToCart component to use cart store
    The AddToCart component in the src/app/(shop)/product/[slug]/ui/AddToCart.tsx file has been updated to utilize the cart store for adding products to the cart. This refactor improves the code by centralizing the cart functionality and ensuring consistency across the application.

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,25 +1,42 @@
 'use client'
 
-import { Product, Size } from '@/interfaces'
+import { CartItem, Product, Size } from '@/interfaces'
 import { QuantitySelector, SizeSelector } from '@/components'
 
-import { useState } from 'react';
+import { useCartStore } from '@/store'
+import { useState } from 'react'
 
 interface Props {
   product: Product
 }
 
 export const AddToCart = ({ product }: Props) => {
+  const addProductToCart = useCartStore((state) => state.addToCart)
 
-
-  const [size, setSize] = useState<Size|undefined>()
+  const [size, setSize] = useState<Size | undefined>()
   const [quantity, setQuantity] = useState<number>(1)
   const [posted, setPosted] = useState<boolean>(false)
 
   const addToCart = () => {
     setPosted(true)
-    if(!size) return
-    console.log('Add to cart', product, size, quantity)
+    if (!size) return
+    // console.log('Add to cart', product, size, quantity)
+    const cartProduct: CartItem = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      size: size,
+      price: product.price,
+      quantity: quantity,
+      image: product.images[0],
+    }
+    
+    addProductToCart({
+      ...cartProduct,
+    })
+    setPosted(false)
+    setSize(undefined)
+    setQuantity(1)
   }
 
   return (
@@ -28,9 +45,11 @@ export const AddToCart = ({ product }: Props) => {
       {/* Size selector */}
       <SizeSelector availableSizes={product.sizes} selectedSize={size} onSizeChange={setSize} />
       {/* Quantity selector */}
-      <QuantitySelector quantity={quantity} onQuantityChange={setQuantity}/>
+      <QuantitySelector quantity={quantity} onQuantityChange={setQuantity} />
       {/* Button */}
-      <button className="btn-primary my-5" onClick={addToCart}>Add to cart</button>
+      <button className="btn-primary my-5" onClick={addToCart}>
+        Add to cart
+      </button>
     </>
   )
 }
