d20d66e666cf426a63afbc0895100accc6aba58d
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 17:49:56 +0100

    ```text refactor: Update TopMenu component to use client for react-icons
    The TopMenu component in the src/components/ui/top-menu/TopMenu.tsx file has been updated to use the "use client" hook for importing the react-icons library. This refactor improves the code consistency by utilizing the recommended approach for importing external libraries and ensures better compatibility with future updates.

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,44 +1,41 @@
-"use client";
+'use client'
 
-import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
+import React, { useEffect, useState } from 'react'
+import { useCartStore, useUiStore } from '@/store'
 
-import Link from "next/link";
-import React from "react";
-import { titleFont } from "@/config/fonts";
-import { useUiStore } from "@/store";
+import Link from 'next/link'
+import { titleFont } from '@/config/fonts'
 
 export const TopMenu = () => {
-  const openMenu = useUiStore((state) => state.openSideMenu);
+  const openMenu = useUiStore((state) => state.openSideMenu)
+  const totalItemsInCart = useCartStore((state) => state.getTotalItems())
+
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
   return (
     <nav className="flex px-5 justify-between items-center w-full">
       {/* {Logo} */}
       <div>
         <Link href="/">
-          <span className={`${titleFont.className} antialiased font-bold`}>
-            Teslo
-          </span>
+          <span className={`${titleFont.className} antialiased font-bold`}>Teslo</span>
           <span> | Shop</span>
         </Link>
       </div>
 
       {/* {Top Menu} */}
       <div className="hidden sm:block">
-        <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href="/gender/men"
-        >
+        <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/gender/men">
           Men
         </Link>
-        <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href="/gender/women"
-        >
+        <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/gender/women">
           Women
         </Link>
-        <Link
-          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href="/gender/kid"
-        >
+        <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/gender/kid">
           Kids
         </Link>
       </div>
@@ -51,20 +48,24 @@ export const TopMenu = () => {
 
         <Link href="/cart">
           <div className="relative">
-            <span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
-              3
-            </span>
+            {
+              // Show the number of items in the cart
+              (loaded && totalItemsInCart > 0) && (
+                <span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
+                  {totalItemsInCart}
+                </span>
+              )
+            }
             <IoCartOutline className="w-5 h-5" />
           </div>
         </Link>
 
         <button
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          onClick={() => openMenu()}
-        >
+          onClick={() => openMenu()}>
           Menu
         </button>
       </div>
     </nav>
-  );
-};
+  )
+}
