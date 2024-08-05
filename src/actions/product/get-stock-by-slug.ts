f895c9426d48ae485fe3ebf77c9a7759e00f6e12
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-05 11:02:10 +0100

    refactor: Add getStockBySlug function to retrieve stock by slug

---
+++ src/actions/product/get-stock-by-slug.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import prisma from '@/lib/prisma'
+
+export const getStockBySlug = async (slug: string): Promise<number> => {
+  try {
+    const stock = await prisma.product.findFirst({
+      where: {
+        slug: slug,
+      },
+      select: {
+        inStock: true,
+      },
+    }) // Add the query to get the stock by slug
+    console.log('stock', stock)
+    return stock?.inStock || 0
+  } catch (error) {
+    console.log('stock en error', error)
+    return 0
+  }
+}
