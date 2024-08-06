60a2631a5625eb795170b38a0c32abef92d41961
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-06 16:58:46 +0100

    feat: Add NextAuth credentials provider for authentication
    The `auth.config.ts` file in the `src` directory has been updated to include the `Credentials` provider from NextAuth. This provider allows users to authenticate using email and password credentials. The `authorize` function has been implemented to handle the authentication logic, parsing and validating the provided credentials. This feature enhances the authentication functionality of the application.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,35 @@
+import Credentials from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth';
 import type { NextAuthConfig } from 'next-auth'
+import SignUp from './app/auth/new-account/page';
+import { z } from 'zod';
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/new-account',
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+          if (!parsedCredentials.success) {
+            return null;
+          }
+
+          const { email, password } = parsedCredentials.data;
+
+          console.log('email', email);
+          console.log('password', password);
+
+
+          return null;
+      },
+    }),
+  ],
 }
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
