198595226bef1838301e2d8008b020a16e58cf66
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-06 12:57:42 +0100

    refactor: Update TopMenu component to conditionally display cart item count
    The TopMenu component in the src/components/ui/top-menu/TopMenu.tsx file has been refactored to conditionally display the cart item count. The Link component's href attribute has been updated to include a condition that checks if the totalItemsInCart is 0 and if the loaded state is true. This change ensures that the cart link is only displayed when there are items in the cart and the page has finished loading. This refactor improves the user experience by providing a more intuitive and responsive navigation menu.

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -46,12 +46,12 @@ export const TopMenu = () => {
           <IoSearchOutline className="w-5 h-5" />
         </Link>
 
-        <Link href={totalItemsInCart === 0 ? '/empty' : '/cart'}>
+        <Link href={(totalItemsInCart === 0 && loaded) ? '/empty' : '/cart'}>
           <div className="relative">
             {
               // Show the number of items in the cart
               loaded && totalItemsInCart > 0 && (
-                <span className="absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
+                <span className="fade-in absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
                   {totalItemsInCart}
                 </span>
               )
