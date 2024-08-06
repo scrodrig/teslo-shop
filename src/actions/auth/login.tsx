d7fe9050197f9e3524616c4106bfcf0d9a6f5dd6
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-06 17:29:29 +0100

    refactor: Improve error handling in authenticate function
    The `authenticate` function in the `login.tsx` file has been refactored to improve error handling. The commented switch statement has been removed and a more robust error handling approach has been implemented using the `AuthError` class. This refactor enhances the reliability and maintainability of the authentication functionality.

---
+++ src/actions/auth/login.tsx
@@ -0,0 +1,24 @@
+'use server'
+
+import { AuthError } from 'next-auth'
+import { signIn } from '@/auth.config'
+
+// ...
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    console.log('formData', {formData: Object.fromEntries(formData)})
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+    //   switch (error.type) {
+    //     case 'CredentialsSignin':
+    //       return 'Invalid credentials.'
+    //     default:
+    //       return 'Something went wrong.'
+    //   }
+    }
+    // throw error
+    return 'CredentialsSignin'
+  }
+}
