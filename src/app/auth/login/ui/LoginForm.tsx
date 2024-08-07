79d53aa257494a30f06065a61f84884b3789c38f
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-07 11:24:50 +0100

    refactor: Update LoginForm component to handle form submission

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+// import { useActionState } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
+
+import { IoInformationOutline } from 'react-icons/io5'
 import Link from 'next/link'
 import { authenticate } from '@/actions'
-// import { useActionState } from 'react'
-import { useFormState } from 'react-dom'
+import clsx from 'clsx'
 
 export const LoginForm = () => {
   // const [errorMessage, formAction, isPending] = useActionState(
@@ -13,17 +16,28 @@ export const LoginForm = () => {
 
   const [state, dispatch] = useFormState(authenticate, undefined)
 
-  console.log('state', {state})
+  console.log('state', { state })
 
   return (
     <form action={dispatch} className="flex flex-col">
       <label htmlFor="email">Email</label>
-      <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="email" name='email' />
+      <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="email" name="email" />
 
       <label htmlFor="email">Password</label>
-      <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="password" name='password' />
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        name="password"
+      />
 
-      <button type='submit' className="btn-primary">Login</button>
+      {state === 'CredentialsSignin' && (
+        <div className="flex flex-row mb-2">
+          <IoInformationOutline className="h-5 w-5 text-red-500" />
+          <p className="text-red-500">Invalid credentials.</p>
+        </div>
+      )}
+
+      <LoginButton />
 
       {/* divisor l ine */}
       <div className="flex items-center my-5">
@@ -36,5 +50,21 @@ export const LoginForm = () => {
         Sign Up
       </Link>
     </form>
+  )
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      type="submit"
+      className={clsx({
+        'btn-primary': !pending,
+        'btn-disabled': pending,
+      })}
+      disabled={pending}>
+      Login
+    </button>
   )
 }
