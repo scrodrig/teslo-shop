46974026cd0cdd603c8c2ebefa01aee651ccc331
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-07 11:57:34 +0100

    feat: Add logout functionality
    This code change adds a new file `logout.ts` in the `src/actions/auth` directory. The `logout.ts` file contains the implementation of the `logout` function, which calls the `signOut` function from the `@/auth.config` module to log the user out. This feature enhances the application by providing a way for users to securely log out of their accounts.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,5 +8,6 @@ import { authenticate } from './auth/login'
 import { getPaginatedProductsWithImages } from './product/get-product-pagination'
 import { getProductbySlug } from './product/get-product-by-slug'
 import { getStockBySlug } from './product/get-stock-by-slug'
+import { logout } from './auth/logout'
 
-export { getPaginatedProductsWithImages, getProductbySlug, getStockBySlug, authenticate }
+export { getPaginatedProductsWithImages, getProductbySlug, getStockBySlug, authenticate, logout }
