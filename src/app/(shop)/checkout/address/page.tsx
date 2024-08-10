1954180a31bd0095761e7dbab69db87870c04cea
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 18:32:52 +0100

    refactor: Update AddressCheckoutPage to fetch countries asynchronously
    This code change updates the AddressCheckoutPage component to fetch the list of countries asynchronously using the getCountry action. The fetched countries are then passed as a prop to the AddressForm component. This refactor improves the performance of the page by fetching the countries separately and ensures that the AddressForm component has the necessary data to render properly.

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,12 +1,16 @@
 import { AddressForm } from './ui/AddressForm'
+import { Country } from '@/interfaces'
 import { Title } from '@/components'
+import { getCountry } from '@/actions'
 
-export default function AddressCheckoutPage() {
+export default async function AddressCheckoutPage() {
+  const countries: Country[] = await getCountry()
+
   return (
     <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
       <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
         <Title title="Address" subtitle="Delivery address" />
-        <AddressForm />
+        <AddressForm countries={countries} />
       </div>
     </div>
   )
