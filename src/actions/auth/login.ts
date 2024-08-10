a6d39f638df7dfe9d3b20d820e24575e009ed810
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 10:02:45 +0100

    refactor: Add login function for user authentication

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -14,3 +14,24 @@ export async function authenticate(prevState: string | undefined, formData: Form
     return 'CredentialsSignin'
   }
 }
+
+
+export async function login(email: string, password: string) {
+  try {
+    await signIn('credentials', {
+      redirect: false,
+      email,
+      password,
+    })
+    return {
+      status: 'success',
+      message: 'User logged in successfully',
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      status: 'error',
+      message: 'Error logging in',
+    }
+  }
+}
