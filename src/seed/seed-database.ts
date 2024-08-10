0bcbb5c337fc0a4e894a984cc990363b59baaa9b
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 18:25:22 +0100

    refactor: Refactor seed-database.ts to improve data seeding process
    This code change refactors the seed-database.ts file to improve the data seeding process. The changes include:
    - Importing the countries data from seed-countries.ts
    - Deleting existing data from the country, productImage, product, category, and user tables
    - Creating new records for countries, categories, and products based on the initialData
    - Mapping category names to category IDs for product creation
    - Creating product images based on the images data
    - Logging a message to indicate the completion of the seed process
    
    These refactorings enhance the efficiency and reliability of the data seeding process, ensuring that the database is properly populated with the initial data.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,62 +1,67 @@
-import { initialData } from "./seed";
-import prisma from "../lib/prisma";
+import { countries } from './seed-countries'
+import { initialData } from './seed'
+import prisma from '../lib/prisma'
 // import prims
 
 async function main() {
   //? Delete data
   // await Promise.all([
-    await prisma.productImage.deleteMany();
-    await prisma.product.deleteMany();
-    await prisma.category.deleteMany();
-    await prisma.user.deleteMany();
+  await prisma.country.deleteMany()
+  await prisma.productImage.deleteMany()
+  await prisma.product.deleteMany()
+  await prisma.category.deleteMany()
+  await prisma.user.deleteMany()
   // ]);
 
   //? Add a category
 
-  const { categories, products, users } = initialData;
+  const { categories, products, users } = initialData
 
-  const categoriesData = categories.map((category) => ({ name: category }));
+  const categoriesData = categories.map((category) => ({ name: category }))
 
   await prisma.user.createMany({
     data: users,
-  });
+  })
+
+  await prisma.country.createMany({
+    data: countries,
+  })
 
   await prisma.category.createMany({
     data: categoriesData,
-  });
+  })
 
-  const categoriesDb = await prisma.category.findMany();
+  const categoriesDb = await prisma.category.findMany()
 
   const categoriesMap = categoriesDb.reduce((map, category) => {
-    map[category.name.toLowerCase()] = category.id;
-    return map;
-  }, {} as Record<string, string>);
-
+    map[category.name.toLowerCase()] = category.id
+    return map
+  }, {} as Record<string, string>)
 
   products.forEach(async (product) => {
-    const { images, type, ...productData } = product;
+    const { images, type, ...productData } = product
     const productDb = await prisma.product.create({
       data: {
         ...productData,
         categoryId: categoriesMap[type.toLowerCase()],
       },
-    });
+    })
     //? Add image
     const imagesData = images.map((image) => ({
       url: image,
       productId: productDb.id,
-    }));
+    }))
 
     await prisma.productImage.createMany({
       data: imagesData,
-    });
-  });
-  
-  console.log("Seed executed...");
+    })
+  })
+
+  console.log('Seed executed...')
 }
 
-(() => {
-  if (process.env.NODE_ENV === "production") return;
+;(() => {
+  if (process.env.NODE_ENV === 'production') return
 
-  main();
-})();
+  main()
+})()
