137ff269a880b60bab3ac9b3f9bb8926361e1904
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 09:31:01 +0100

    feat: Create RegisterForm component for new account registration
    This code change adds a new component called RegisterForm in the file `src/app/auth/new-account/ui/RegisterForm.tsx`. The RegisterForm component includes form inputs for name, email, and password, along with validation using the react-hook-form library. When the form is submitted, the data is logged to the console. This feature enables users to create a new account by filling out the registration form.

---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import Link from 'next/link'
+
+type FormInputs = {
+  name: string
+  email: string
+  password: string
+}
+
+export const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>()
+
+  // const onSubmit = async (data: FormInputs) => {
+  const onSubmit: SubmitHandler<FormInputs> = async (data: FormInputs) => {
+    const { name, email, password } = data
+    console.log('🚀 ~ constonSubmit:SubmitHandler<FormInputs>= ~ { name, email, password }:', {
+      name,
+      email,
+      password,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      {errors.name?.type === 'required' && (
+        <span className="text-red-500">* This field is required</span>
+      )}
+
+      <label htmlFor="email">Full name</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="text"
+        autoFocus
+        {...register('name', { required: true })}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        {...register('email', { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g })}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        {...register('password')}
+      />
+
+      <button className="btn-primary">Create Account</button>
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/login" className="btn-secondary text-center">
+        Sign In
+      </Link>
+    </form>
+  )
+}
