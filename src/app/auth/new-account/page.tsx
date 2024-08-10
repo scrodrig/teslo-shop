9ec1a397e99d4af83baafa381f66ee3b836fdf34
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 09:31:05 +0100

    refactor: Extract RegisterForm component for new account registration
    The code changes in `src/app/auth/new-account/page.tsx` extract the registration form into a separate component called RegisterForm. This refactor improves code organization and reusability by separating the form logic and UI from the main page component.

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,45 +1,11 @@
-import Link from "next/link";
-import { titleFont } from "@/config/fonts";
+import { RegisterForm } from './ui/RegisterForm'
+import { titleFont } from '@/config/fonts'
 
 export default function SignUp() {
   return (
     <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
       <h1 className={`${titleFont.className} text-4xl mb-5`}>Sign up</h1>
-
-      <div className="flex flex-col">
-
-      <label htmlFor="email">Full name</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="text"
-        />
-
-
-        <label htmlFor="email">Email</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
-        />
-
-        <label htmlFor="email">Password</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
-        />
-
-        <button className="btn-primary">Create Account</button>
-
-        {/* divisor l ine */}
-        <div className="flex items-center my-5">
-          <div className="flex-1 border-t border-gray-500"></div>
-          <div className="px-2 text-gray-800">O</div>
-          <div className="flex-1 border-t border-gray-500"></div>
-        </div>
-
-        <Link href="/auth/login" className="btn-secondary text-center">
-          Sign In
-        </Link>
-      </div>
+      <RegisterForm />
     </div>
-  );
+  )
 }
