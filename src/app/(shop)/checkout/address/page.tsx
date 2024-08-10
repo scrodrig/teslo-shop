09579db821939f23ff4d60e0374c72ff6a92df44
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-10 17:59:50 +0100

    refactor: Add AddressForm component for checkout
    The code changes in `src/app/(shop)/checkout/address/page.tsx` refactor the checkout address page by replacing the individual input fields with a reusable AddressForm component. This improves code organization and maintainability by encapsulating the address input logic into a separate component. The AddressForm component includes fields for names, last names, address, address 2 (optional), ZIP code, city, country, and phone. This refactor enhances the user experience by providing a consistent and streamlined way for users to input their address information during checkout.

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,66 +1,13 @@
-import Link from "next/link";
-import { Title } from "@/components";
+import { AddressForm } from './ui/AddressForm'
+import { Title } from '@/components'
 
 export default function AddressCheckoutPage() {
   return (
     <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
       <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
         <Title title="Address" subtitle="Delivery address" />
-
-        <div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
-          <div className="flex flex-col mb-2">
-            <span>Names</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>Last Names</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>Address</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>Address 2 (opcional)</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>ZIP Code</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>City</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>Country</span>
-            <select className="p-2 border rounded-md bg-gray-200">
-              <option value="">[ Select ]</option>
-              <option value="PT">Portugal</option>
-            </select>
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <span>Phone</span>
-            <input type="text" className="p-2 border rounded-md bg-gray-200" />
-          </div>
-
-          <div className="flex flex-col mb-2 sm:mt-10">
-            <Link
-              href="/checkout"
-              className="btn-primary flex w-full sm:w-1/2 justify-center "
-            >
-              Next
-            </Link>
-          </div>
-        </div>
+        <AddressForm />
       </div>
     </div>
-  );
+  )
 }
