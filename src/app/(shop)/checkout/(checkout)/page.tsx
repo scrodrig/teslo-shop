6d9ce1360938ddb3fe9382ea3af372b62f67a05b
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-12 22:12:32 +0100

    refactor: Extract PlaceOrder component in CheckoutPage
    This code change refactors the CheckoutPage component by extracting the PlaceOrder component into a separate file. By doing this, the code becomes more modular and easier to maintain. This enhancement improves the readability and reusability of the code.

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { PlaceOrder } from './ui/PlaceOrder'
 import { ProductsInCart } from './ui/ProductsInCart'
 import { Title } from '@/components'
 
@@ -19,47 +20,7 @@ export default function CheckoutPage() {
             <ProductsInCart />
           </div>
           {/* Checkout */}
-          <div className="bg-white rounded-xl shadow-xl p-7">
-            <h2 className="text-2xl mb-2">Delivery address</h2>
-            <div className="mb-10">
-              <p className="text-xl">John Doe</p>
-              <p>Av. Siempre Viva 123</p>
-              <p>Col. Central</p>
-              <p>Alcaldia Cauctemoc</p>
-              <p>Porto</p>
-              <p>4000-123</p>
-            </div>
-
-            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"></div>
-
-            <h2 className="text-2xl mb-2">Order Summary</h2>
-            <div className="grid grid-cols-2">
-              <span>No. Productos</span>
-              <span className="text-right">3 articles</span>
-
-              <span>Subtotal</span>
-              <span className="text-right">$100</span>
-
-              <span>Impuestos</span>
-              <span className="text-right">$23</span>
-
-              <span className="text-xl font-bold mt-5">Total:</span>
-              <span className="text-xl mt-5 text-right">$123</span>
-            </div>
-
-            <p className="text-xs">
-              {`By clicking on Place Order" you agree to our `}
-              <Link href="#" className="underline">
-                terms and conditions
-              </Link>
-            </p>
-
-            <div className="mt-5 mb-2 w-full">
-              <Link href="/orders/123" className="flex justify-center btn-primary">
-                Place Order
-              </Link>
-            </div>
-          </div>
+          <PlaceOrder />
         </div>
       </div>
     </div>
