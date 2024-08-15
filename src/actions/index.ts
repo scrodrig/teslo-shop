9be89d57ccac7a77aefde3500c3b1e59094cc0f1
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-15 12:51:15 +0100

    feat: Add setTransactionId function for updating transaction ID

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,15 +7,16 @@
 import { authenticate } from './auth/login'
 import { deleteUserAddress } from './address/delete-user-address'
 import { getCountry } from './country/get-country'
-import { getOrderByID } from './order/get-order-by-id';
-import { getOrderByUserId } from './order/get-orders-by-user';
+import { getOrderByID } from './order/get-order-by-id'
+import { getOrderByUserId } from './order/get-orders-by-user'
 import { getPaginatedProductsWithImages } from './product/get-product-pagination'
 import { getProductbySlug } from './product/get-product-by-slug'
 import { getStockBySlug } from './product/get-stock-by-slug'
 import { getUserAddress } from './address/get-user-address'
 import { logout } from './auth/logout'
-import { placeOrder } from './order/place-order';
+import { placeOrder } from './order/place-order'
 import { registerUser } from './auth/register'
+import { setTransactionId } from './payments/set-transaction-id'
 import { setUserAddress } from './address/set-user-address'
 
 export {
@@ -31,5 +32,6 @@ export {
   getUserAddress,
   placeOrder,
   getOrderByID,
-  getOrderByUserId
+  getOrderByUserId,
+  setTransactionId,
 }
