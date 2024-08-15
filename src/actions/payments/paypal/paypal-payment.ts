8169c8720f9c36fbbcffcad1ba2049674227a4f4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-15 14:46:09 +0100

    feat: Add PayPal bearer token retrieval function
    This code change adds the `getPayPalBearerToken` function to the `paypal-payment.ts` file. This function retrieves the bearer token required for authentication with the PayPal API. It uses the client ID and secret from the environment variables to make a POST request to the PayPal OAuth endpoint and returns the access token. This enhancement improves the payment processing capabilities of the application by ensuring the availability of the necessary authentication token.

--- src/actions/payments/paypal/paypal-payment.ts
+++ src/actions/payments/paypal/paypal-payment.ts
@@ -1,5 +1,47 @@
 'use server'
 
 export const paypalCheckPayment = async (transactionId?: string) => {
+  const authToken = await getPayPalBearerToken()
+  
+  if (!authToken) {
+    return {
+      success: false,
+      message: 'Error getting PayPal token',
+    }
+  }
+  
+  console.log("🚀 ~ paypalCheckPayment ~ authToken:", authToken)
   console.log('🚀 ~ paypalCheckPayment ~ transactionId:', transactionId)
+}
+
+const getPayPalBearerToken = async (): Promise<string | null> => {
+  const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
+  const PAYPAL_CLIENT_SECRET = process.env.PAYPAL_SECRET
+  const oauth2Url = process.env.PAYPAL_OAUTH_URL ?? ''
+
+  const base64Token = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`, 'utf-8').toString(
+    'base64'
+  )
+
+  const headersList = {
+    Accept: '*/*',
+    Authorization: `Basic ${base64Token}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
+
+  const bodyContent = 'grant_type=client_credentials'
+
+  const resultOptions = {
+    method: 'POST',
+    body: bodyContent,
+    headers: headersList,
+  }
+
+  try {
+    const response = await fetch(oauth2Url, resultOptions).then((res) => res.json())
+    return response.access_token
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
