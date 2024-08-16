a6165ddccae40a9b95359a70e5c55da1a37ffc95
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-17 00:42:58 +0100

    feat: Add users administration page
    This code change adds a new page component for the users administration page. The page displays a table of users with their details and includes pagination functionality. It also includes a redirect to the login page if the user is not authenticated. This addition enhances the application by providing a dedicated page for admins to view and manage users.

---
+++ src/app/(shop)/admin/users/page.tsx
@@ -0,0 +1,24 @@
+// https://tailwindcomponents.com/component/hoverable-table
+import { Pagination, Title } from '@/components'
+
+import { UsersTable } from './ui/UsersTable'
+import { getPaginatedUsers } from '@/actions'
+import { redirect } from 'next/navigation'
+
+export default async function UsersPage() {
+  const { success, users= [] } = await getPaginatedUsers()
+
+  if (!success) {
+    redirect('/auth/login')
+  }
+
+  return (
+    <>
+      <Title title="Users administrations" />
+      <div className="mb-10">
+        <UsersTable users={users} />
+        <Pagination totalPages={3} />
+      </div>
+    </>
+  )
+}
