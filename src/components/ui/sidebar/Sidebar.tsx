f8e700b7747bad15224cb3e655feb7af031240bb
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-16 23:57:32 +0100

    refactor: Update sidebar link to admin/orders

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -122,7 +122,8 @@ export const Sidebar = () => {
             </Link>
 
             <Link
-              href="/orders"
+              href="/admin/orders"
+              onClick={() => closeMenu()}
               className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
               <IoTicketOutline size={30} />
               <span className="ml-3 text-xl">Orders</span>
