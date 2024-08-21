a6bf8da886e0076188d9df353584250bf2110772
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-21 23:16:04 +0100

    feat: Add ProductImage component to ProductsPage
    This code change adds the ProductImage component to the ProductsPage in order to display the product images. It replaces the previous usage of the Image component and includes additional styling for the image size and shape. This enhancement improves the visual presentation of the product images on the page.

--- src/app/(shop)/admin/products/page.tsx
+++ src/app/(shop)/admin/products/page.tsx
@@ -1,5 +1,5 @@
 // https://tailwindcomponents.com/component/hoverable-table
-import { Pagination, Title } from '@/components'
+import { Pagination, ProductImage, Title } from '@/components'
 import { getOrderByUserId, getPaginatedOrders, getPaginatedProductsWithImages } from '@/actions'
 
 import { Gender } from '@prisma/client'
@@ -64,11 +64,12 @@ export default async function ProductsPage({ searchParams }: Props) {
                 className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                   <Link href={`/product/${product.slug}`}>
-                    <Image
-                      src={`/products/${product.ProductImage[0].url}`}
+                    <ProductImage
+                      src={product.ProductImage[0]?.url}
                       width={80}
                       height={80}
                       alt={product.title}
+                      className='w-20 h-20 object-cover rounded'
                     />
                   </Link>
                 </td>
