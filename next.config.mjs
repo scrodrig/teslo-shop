acabd81cdea604d09f8a64d7491a4103e7be0f76
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-22 00:13:26 +0100

    feat: Add remotePatterns configuration for Cloudinary image URLs
    This code change adds the `remotePatterns` configuration to the `images` object in the `next.config.mjs` file. The `remotePatterns` array contains an object with the `protocol` and `hostname` properties set to `'https'` and `'res.cloudinary.com'` respectively. This enhancement enables the application to handle Cloudinary image URLs and ensures proper image management and delivery when using Cloudinary as the image hosting platform.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   // compiler: {
   //   styledComponents: true
   // }
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
