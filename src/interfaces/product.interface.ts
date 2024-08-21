3a8dbfa339cf4e8eaaf94cd82f7aea85cc8b9a0e
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-21 19:31:59 +0100

    feat: Add Category interface
    This code change adds the Category interface, which defines the structure of a category object with an id and name. This addition enhances the application by providing a standardized way to represent and handle categories.

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -22,7 +22,7 @@ export interface CartItem {
   image: string;
 }
 
-export type Category = "men" | "women" | "kid" | "unisex";
+type Category = "men" | "women" | "kid" | "unisex";
 export type Size = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 export type Type = "shirts" | "pants" | "hoodies" | "hats";
 export interface SeedData {
