bba0cdaa7a49748ca61683eab3ad52b45768f7d2
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-22 00:26:48 +0100

    feat: Add deleteProductImage function for removing product images

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,20 +6,21 @@
 
 import { authenticate } from './auth/login'
 import { changeUserRole } from './user/change-user-role'
-import { createUpdateProduct } from './product/create-update-product';
+import { createUpdateProduct } from './product/create-update-product'
+import { deleteProductImage } from './product/delete-product-image'
 import { deleteUserAddress } from './address/delete-user-address'
-import { getCategories } from './categories/get-categories';
+import { getCategories } from './categories/get-categories'
 import { getCountry } from './country/get-country'
 import { getOrderByID } from './order/get-order-by-id'
 import { getOrderByUserId } from './order/get-orders-by-user'
-import { getPaginatedOrders } from './order/get-paginated-orders';
+import { getPaginatedOrders } from './order/get-paginated-orders'
 import { getPaginatedProductsWithImages } from './product/get-product-pagination'
 import { getPaginatedUsers } from './user/get-paginated-users'
 import { getProductbySlug } from './product/get-product-by-slug'
 import { getStockBySlug } from './product/get-stock-by-slug'
 import { getUserAddress } from './address/get-user-address'
 import { logout } from './auth/logout'
-import { paypalCheckPayment } from './payments/paypal/paypal-payment';
+import { paypalCheckPayment } from './payments/paypal/paypal-payment'
 import { placeOrder } from './order/place-order'
 import { registerUser } from './auth/register'
 import { setTransactionId } from './payments/set-transaction-id'
@@ -46,4 +47,5 @@ export {
   changeUserRole,
   getCategories,
   createUpdateProduct,
+  deleteProductImage,
 }
