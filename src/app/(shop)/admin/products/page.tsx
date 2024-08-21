f33867976291e0e60a0b781593af463647179fc1
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-21 18:43:43 +0100

    feat: Add product management page
    This code change adds the `ProductsPage` component, which is responsible for rendering the page for managing products in the admin section of the application. It retrieves paginated products with images from the database and displays them in a table. The page also includes a "New Product" button for creating a new product. This addition enhances the application by providing a dedicated page for managing products and improves the user experience for admins.

---
+++ src/app/(shop)/admin/products/page.tsx
@@ -0,0 +1,106 @@
+// https://tailwindcomponents.com/component/hoverable-table
+import { Pagination, Title } from '@/components'
+import { getOrderByUserId, getPaginatedOrders, getPaginatedProductsWithImages } from '@/actions'
+
+import { Gender } from '@prisma/client'
+import Image from 'next/image'
+import { IoCardOutline } from 'react-icons/io5'
+import Link from 'next/link'
+import { currencyFormater } from '@/utils'
+import { redirect } from 'next/navigation'
+
+interface Props {
+  searchParams: {
+    page?: number
+  }
+}
+
+export default async function ProductsPage({ searchParams }: Props) {
+  const page = searchParams.page ? Number(searchParams.page) : 1
+
+  const { products, currentPage, totalPages } = await getPaginatedProductsWithImages({
+    page,
+  })
+
+  const { success, orders = [] } = await getPaginatedOrders()
+
+  return (
+    <>
+      <Title title="Products management" />
+
+      <div className="flex justify-end mb-5">
+        <Link href="/admin/product/new" className="btn-primary">
+          New Product
+        </Link>
+      </div>
+
+      <div className="mb-10">
+        <table className="min-w-full">
+          <thead className="bg-gray-200 border-b">
+            <tr>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Image
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Title
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Price
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Gender
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Stock
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Sizes
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products!.map((product) => (
+              <tr
+                key={product.id}
+                className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  <Link href={`/product/${product.slug}`}>
+                    <Image
+                      src={`/products/${product.ProductImage[0].url}`}
+                      width={80}
+                      height={80}
+                      alt={product.title}
+                    />
+                  </Link>
+                </td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  <Link href={`/admin/product/${product.slug}`} className="hover:underline">
+                    {product.title}
+                  </Link>
+                </td>
+
+                <td className="text-sm text-gray-900 font-bold px-6 ">
+                  {currencyFormater(product.price)}
+                </td>
+
+                <td className="text-sm text-gray-900 font-light px-6 ">
+                  {product.gender}
+                </td>
+
+                <td className="text-sm text-gray-900 font-bold px-6 ">
+                  {product.inStock}
+                </td>
+
+                <td className="text-sm text-gray-900 font-bold px-6 ">
+                  {product.sizes.join(', ')}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Pagination totalPages={totalPages} />
+      </div>
+    </>
+  )
+}
